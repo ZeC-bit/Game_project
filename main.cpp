4c1a9179ef9597801635b8cf49cf73b38323d3f7
hybp <hybp@hku.hk>
2021-11-21 22:03:00 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,5 +7,13 @@ void introduction() {
     string name;
     cout << "What is your name?";
     cin >> name;
-    
+
+}
+
+int main() {
+    int dummy;
+    cout << "HB and King SH presents" << endl;
+    cin >> dummy;
+    cout << "\"Under the Mistletoe\"" << endl;
+    return 0;
 }
