9797c03185fa07d0ef81284bf613b6901d70f31a
hybp <hybp@hku.hk>
2021-11-28 21:27:53 +0800

    Update save_load.cpp

--- save_load.cpp
+++ save_load.cpp
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-void save_data(status stat_save) 
+void save_data(status &stat_save)
 {
     string flag;
     cout << "Do you want to save current progress?";
     getchar();
-    cout << "Press 'y' if you want to save progress. Press 'n' if you do not want to save.";
+    cout << "Press 'y' if you want to save progress. Press 'n' if you do not wish to save.";
     
     cin >> flag;
     if (flag == "n" || flag == "N") {
@@ -18,15 +18,12 @@ void save_data(status stat_save)
     }
     
     cout << "Enter name for your save file.\n";
-    ofstream dout;
+    ofstream d_out;
     string save_name;
     cin >> save_name;
     save_name += ".txt";
-    dout.open(save_name.c_str());
-    dout << stat_save.c_name << endl << 
-
+    d_out.open(save_name.c_str());
+    d_out << stat_save.c_name << endl << stat_save.c_date << endl << stat_save.c_time << endl << stat_save.c_location << endl << stat_save.c_aff_level << endl << stat_save.c_int_level << endl << stat_save.c_GPA << endl << stat_save.c_health << endl;
+    cout << "Your current progress has been saved at \"" << save_name << ".txt!\"\n";
+    d_out.close();
 }
-
-void load_data(status stat_load) 
-{
-    
