8c92a5fe40fc87d75f2e66e85e474951ff1d9be2
ZeC-bit <74304944+ZeC-bit@users.noreply.github.com>
2021-12-02 15:13:21 +0800

    Update cafeteria.cpp

--- cafeteria.cpp
+++ cafeteria.cpp
@@ -10,14 +10,13 @@ using namespace std;
 //play.period ==> 1. morning 2. afternoon 3. evening
 //play.location ==> 0. Library 1. Garden 2. Student Cafeteria  3. Lecture Room
 // Iris = [0], Olivia = [1], Daisy = [2]
-
-
 void cafeteria(status* input_stat, int mood){
   char choose;
   int input;
   char name;
   double increment = 1;
   double decrement = -1;
+  int sun;
   if (mood == 0){
     decrement = -1.5;
     increment = 0.8;
@@ -35,75 +34,96 @@ void cafeteria(status* input_stat, int mood){
       cin >> sel;
       if(sel ==1){
         srand(time(0));
+        // Daisy Appears on Cafeteria
         int Daisy_Appear = rand() % 100;
-        if(Daisy_Appear < 70){
+        if(Daisy_Appear < 50){
           cout << "Daisy appears on the Cafeteria" << endl;
+          cout << "Hi!! I'm Daisy~~~ " << endl;
           getchar();
           cout << "Getting hungry, you went to have some food." << endl;
           cout << "You saw Daisy messed up the midterm exam and found out that she is below the average score. " << endl;
           getchar();
           cout << " You may choose between three options: "<< endl;
-          cout << " 1. Ask why she is crying. " << endl;
-          cout << " 2. Comfort her without saying any word. " << endl;
-          cout << " 3. Didn't recognize Olivia crying and look around Garden. " << endl;
+          cout << " 1. It is fine, Daisy. GPA is not all of your life, you can do it better next time!! " << endl;
+          cout << " 2. Give her a can of Coke and hear what she says. " << endl;
+          cout << " 3. HAHAAHAHAHAAHA (Laugh Out Loud) " << endl;
           cout << "please enter a number from 1 to 3" << endl;
           cin >> input;
           if(input == 1){
-            cout << "Olivia: life is hard cuz of too much of tests and assignments..." << endl;
-            input_stat->inti_level[1] += increment;
-          }
-          else if(input == 2){
-            cout << "Thank you for comforting me.." << endl;
+            cout << " Olivia: Thank you a lot for your comfort!!! I will do it better!! " << endl;
             input_stat->affi_level[1] += increment;
+            input_stat->sun += 1;
+          }
+          else if(input == 2){
+            cout << " Olivia: Thank you for the drink! I will buy you one day, haha " << endl;
+            input_stat->inti_level[1] += increment;
+            input_stat->sun += 1;
           }
           else{
-            cout << "(Nothing happens)" << endl;
-            input_stat->inti_level[1] -= decrement;
-            input_stat->affi_level[1] -= decrement;
+            cout << " (You saw Daisy with furious face) Daisy:......(Left Cafeteria) " << endl;
+            input_stat->inti_level[1] += decrement;
+            input_stat->affi_level[1] += decrement;
+            input_stat->sun += 1;
           }
         }
-        else if(Daisy_Appear > 70 && Daisy_Appear < 79){
-          cout<< "Iris appears on the garden" << endl;
+        // Iris appears on the Cafeteria
+        else if(Daisy_Appear > 50 && Daisy_Appear < 69){
+          cout<< "Iris appears on the cafeteria" << endl;
+          cout << "Iris: Hi, I'm Iris!" << endl;
           getchar();
-          cout << " 1. Take the flower to give her " << endl;
-          cout << " 2. Since you consider every living thing a precious one, you keep walking to the garden " << endl;
-          cout << " 3. Buy an Oreo Ice cream to her and walk with her on the Garden " << endl;
+          cout << "You went to the Cafeteria with Iris and found out that there was a cockroach inside the dish. " << endl;
+          getchar();
+          cout << " 1. Leave the Cafeteria and buy her a nice meal. " << endl;
+          cout << " 2. Throw the cockroach away and have the meal as if nothing happened. " << endl;
+          cout << " 3. Go fight for the peace of Student Cafeteria!! " << endl;
           cout << "please enter a number from 1 to 3" << endl;
           cin >> input;
           if(input == 1){
-            cout << " Iris: (With impression) I liked this kind of flower..Thank you!!" << endl;
+            cout << " Iris: It was such a terrible experience. I won't go there again!! " << endl;
+            cout << " Iris: Btw Thank you for the meal!! " << endl;
             input_stat->affi_level[0] += increment;
+            input_stat->sun += 1;
           }
           else if(input == 2){
-            cout << " Iris: .... " << endl;
-            input_stat->inti_level[0] -= decrement;
-            input_stat->affi_level[0] -= decrement;
+            cout << " Iris: ....(Left Cafeteria) " << endl;
+            input_stat->inti_level[0] += decrement;
+            input_stat->affi_level[0] += decrement;
+            input_stat->sun += 1;
           }
           else{
-            cout << " Iris: It is really nice of you for this Oreo icecream! let's walk around!! " << endl;
+            cout << " Iris: Thank you for your courage...I don't wanna go there agian... " << endl;
             input_stat->inti_level[0] += 1;
+            input_stat->sun += 1;
           }
         }
-        else if(Daisy_Appear > 80 && Daisy_Appear < 90){
-          cout << "Daisy appears on the library" << endl;
+        // Olivia Appears on the cafeteria
+        else if(Daisy_Appear > 70 && Daisy_Appear < 90){
+          cout << "Olivia appears on the cafeteria" << endl;
+          cout << " Hi, I'm Olivia. " << endl;
           getchar();
-          cout << " 1. Take the flower to give her " << endl;
-          cout << " 2. Since you consider every living thing a precious one, you keep walking to the garden " << endl;
-          cout << " 3. Buy an Oreo Ice cream to her and walk with her on the Garden " << endl;
+          cout << "You went to the Cafeteria with Olivia and found out that there was a cockroach inside the dish. " << endl;
+          getchar()l
+          cout << " 1. Leave the Cafeteria and buy her a nice meal. " << endl;
+          cout << " 2. Throw the cockroach away and have the meal as if nothing happened. " << endl;
+          cout << " 3. Go fight for the peace of Student Cafeteria!! " << endl;
           cout << "please enter a number from 1 to 3" << endl;
           cin >> input;
           if(input == 1){
-            cout << " Daisy: (With impression) I liked this kind of flower..Thank you!!" << endl;
+            cout << " Olivia: It was such a terrible experience. I won't go there again!! " << endl;
+            cout << " Olivia: Btw Thank you for the meal!! " << endl;
             input_stat->affi_level[2] += increment;
+            input_stat->sun += 1;
           }
           else if(input == 2){
-            cout << " Daisy: .... " << endl;
-            input_stat->inti_level[2] -= decrement;
-            input_stat->affi_level[2] -= decrement;
+            cout << " Olivia: ....(Left Cafeteria) " << endl;
+            input_stat->inti_level[2] += decrement;
+            input_stat->affi_level[2] += decrement;
+            input_stat->sun += 1;
           }
           else{
-            cout << " It is really nice of you for this Oreo icecream! let's walk around!! " << endl;
+            cout << " Olivia: Thank you for your courage...I don't wanna go there agian... " << endl;
             input_stat->inti_level[2] += increment;
+            input_stat->sun += 1;
           }
         }
         else
@@ -111,7 +131,9 @@ void cafeteria(status* input_stat, int mood){
           cout << "Prof T.W. Chim appears" << endl;
           getchar();
           cout << " Not much time for the class until Christmas! " << endl;
-          cout << " So, I prepare you guys with an easy level quiz! ‘cin’ is? " << endl;
+          cout << " Have a good meal guys HAHAHAH " << endl;
+          cout << " I would recommend Donburi Dish in SU HAHAHA " << endl;
+          cout << " And, I prepare you guys with an easy level quiz! ‘cin’ is? " << endl;
           cout << " " << endl;
           cout << "1.	an Object " << endl;
           cout << "2. a Package " << endl;
@@ -121,28 +143,37 @@ void cafeteria(status* input_stat, int mood){
           {
             cout << " Correct!!! You have gained 0.5 input_stat->GPA points. " << endl;
             input_stat->GPA += 0.5;
+            input_stat->sun += 1;
           }
           else if(input ==2)
           {
             cout << " Wrong! You have lost 0.5 input_stat->GPA points. "   << endl;
             input_stat->GPA -= 0.5;
+            input_stat->sun += 1;
           }
           else
           {
             cout << " Wrong! You have lost 0.5 input_stat->GPA points. " << endl;
             input_stat->GPA -= 0.5;
+            input_stat->sun += 1;
           }
         }
           input_stat->hp -= 1;
+          input_stat->GPA -= 0.3;
         }
       else if(sel == 2){
           cout << "Going back home. " << endl;
           input_stat->hp -= 1;
+          input_stat->sun += 1;
+          input_stat->GPA -= 0.1;
         }
       else if(sel == 3){
           cout << " You entered Badminton Club. " << endl;
           cout << " By playing badminton you can gain health point per match! " << endl;
           input_stat->hp += 1;
+          input_stat->sun += 1;
+          input_stat->GPA -= 0.1;
         }
+    run = 0;
     }
   }
