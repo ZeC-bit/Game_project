9bd17d6e20706b85e73a219c92a49687911a6516
ZeC-bit <74304944+ZeC-bit@users.noreply.github.com>
2021-12-02 15:13:30 +0800

    Update garden.cpp

--- garden.cpp
+++ garden.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <iomanip>
 #include <time.h>
 #include <string>
 #include <cstdlib>
@@ -10,13 +11,13 @@ using namespace std;
 //play.period ==> 1. morning 2. afternoon 3. evening
 //play.location ==> 0. Library 1. Garden 2. Student Cafeteria  3. Lecture Room
 // Iris = [0], Olivia = [1], Daisy = [2]
-
 void garden(status* input_stat, int mood){
   char choose;
   int input;
   char name;
   double increment = 1;
   double decrement = -1;
+  int sun;
   if (mood == 0){
     decrement = -1.5;
     increment = 0.8;
@@ -35,8 +36,9 @@ void garden(status* input_stat, int mood){
       if(sel ==1){
         srand(time(0));
         int Olivia_Appear = rand() % 100;
-        if(Olivia_Appear < 70){
+        if(Olivia_Appear < 50){
           cout << "Olivia appears on the Garden" << endl;
+          cout << " Hi, I'm Olivia. " << endl;
           getchar();
           cout << "Stressed out due to excessive amount of assignments and examinations," << endl;
           cout << "You went to Garden to chill. And you found Olivia crying on the bench. " << endl;
@@ -50,19 +52,24 @@ void garden(status* input_stat, int mood){
           if(input == 1){
             cout << "Olivia: life is hard cuz of too much of tests and assignments..." << endl;
             input_stat->inti_level[1] += increment;
+            input_stat->sun += 1;
           }
           else if(input == 2){
             cout << "Thank you for comforting me.." << endl;
             input_stat->affi_level[1] += increment;
+            input_stat->sun += 1;
           }
           else{
             cout << "(Nothing happens)" << endl;
-            input_stat->inti_level[1] -= decrement;
-            input_stat->affi_level[1] -= decrement;
+            input_stat->inti_level[1] += decrement;
+            input_stat->affi_level[1] += decrement;
+            input_stat->sun += 1;
           }
         }
-        else if(Olivia_Appear > 70 && Olivia_Appear < 79){
-          cout<< "Iris appears on the garden" << endl;
+        // random chances for Iris to appear
+        else if(Olivia_Appear > 50 && Olivia_Appear < 69){
+          cout<< "Iris appears on the Garden" << endl;
+          cout << "Iris: Hi, I'm Iris!" << endl;
           getchar();
           cout << " 1. Take the flower to give her " << endl;
           cout << " 2. Since you consider every living thing a precious one, you keep walking to the garden " << endl;
@@ -72,19 +79,23 @@ void garden(status* input_stat, int mood){
           if(input == 1){
             cout << " Iris: (With impression) I liked this kind of flower..Thank you!!" << endl;
             input_stat->affi_level[0] += increment;
+            input_stat->sun += 1;
           }
           else if(input == 2){
             cout << " Iris: .... " << endl;
-            input_stat->inti_level[0] -= decrement;
-            input_stat->affi_level[0] -= decrement;
+            input_stat->inti_level[0] += decrement;
+            input_stat->affi_level[0] += decrement;
+            input_stat->sun += 1;
           }
           else{
             cout << " It is really nice of you for this Oreo icecream! let's walk around!! " << endl;
             input_stat->inti_level[0] += increment;
+            input_stat->sun += 1;
           }
         }
-        else if(Olivia_Appear > 80 && Olivia_Appear < 90){
-          cout << "Daisy appears on the library" << endl;
+        else if(Olivia_Appear > 70 && Olivia_Appear < 90){
+          cout << "Daisy appears on the Garden" << endl;
+          cout << "Hi!! I'm Daisy~~~ " << endl;
           getchar();
           cout << " 1. Take the flower to give her " << endl;
           cout << " 2. Since you consider every living thing a precious one, you keep walking to the garden " << endl;
@@ -94,22 +105,27 @@ void garden(status* input_stat, int mood){
           if(input == 1){
             cout << " Daisy: (With impression) I liked this kind of flower..Thank you!!" << endl;
             input_stat->affi_level[2] += increment;
+            input_stat->sun += 1;
           }
           else if(input == 2){
             cout << " Daisy: .... " << endl;
-            input_stat->inti_level[2] -= decrement;
-            input_stat->affi_level[2] -= decrement;
+            input_stat->inti_level[2] += decrement;
+            input_stat->affi_level[2] += decrement;
+            input_stat->sun += 1;
           }
           else{
             cout << " It is really nice of you for this Oreo icecream! let's walk around!! " << endl;
             input_stat->inti_level[2] += increment;
+            input_stat->sun += 1;
           }
         }
         else
         {
-          cout << "Prof T.W. Chim appears" << endl;
+          cout << "Prof T.W. Chim appears on the Garden" << endl;
           getchar();
-          cout << " Today I will give you guys a question!" << endl;
+          cout << " Nice to see you guys in the Garden!! Hahaha " << endl;
+          cout << " You wanna walk around with me?? HAHAHA " << endl;
+          cout << " I will give you guys a question!" << endl;
           getchar();
           cout << " Guess if you do not know the answer. Which of the following is valid command or editor " << endl;
           cout << " that allow you to show the content of a file in Linux/Unix? " << endl;
@@ -121,28 +137,37 @@ void garden(status* input_stat, int mood){
           {
             cout << " Correct!!! You have gained 0.5 points. " << endl;
             input_stat->GPA += 0.5;
+            input_stat->sun += 1;
           }
           else if(input ==2)
           {
             cout << " Wrong! You have lost 0.5 input_stat->GPA points. "   << endl;
             input_stat->GPA -= 0.5;
+            input_stat->sun += 1;
           }
           else
           {
             cout << " Wrong! You have lost 0.5 input_stat->GPA points. " << endl;
             input_stat->GPA -= 0.5;
+            input_stat->sun += 1;
           }
         }
           input_stat->hp -= 1;
+          input_stat->GPA -= 0.3;
         }
       else if(sel == 2){
           cout << "Going back home. " << endl;
           input_stat->hp -= 1;
+          input_stat->sun += 1;
+          input_stat->GPA -= 0.1;
           }
       else if(sel == 3){
           cout << " You entered Badminton Club. " << endl;
           cout << " By playing badminton you can gain health point per match! " << endl;
           input_stat->hp += 1;
+          input_stat->sun += 1;
+          input_stat->GPA -= 0.1;
           }
+      run = 0;
       }
     }
