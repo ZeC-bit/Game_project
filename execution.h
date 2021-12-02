2c1b32843c3e97ba8b37b4579e528c4c16337e1a
hybp <hybp@hku.hk>
2021-12-02 19:55:39 +0800

    Update execution.h

--- execution.h
+++ execution.h
@@ -2,7 +2,7 @@
 #define EXECUTION_H
 #include "universal.h"
 
-void execution(status& stat_in);
+void execution(status stat_in);
 void decision (status& pass_stat, int pass_mood, bool *qt);
 
 #endif
